d88eea034d4b0b88e6220580995e2a7501ce917a
dtan4 <dtanshi45@gmail.com>
2014-07-01 15:36:49 +0900

    Create controller: welcome

---
+++ spec/controllers/welcome_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe WelcomeController do
+  describe "GET #index" do
+    it "should success" do
+      get :index
+      expect(response).to be_success
+    end
+  end
+end
