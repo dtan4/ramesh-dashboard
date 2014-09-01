71229812302186ac489feead289b1d093b386a5b
dtan4 <dtanshi45@gmail.com>
2014-09-02 02:45:28 +0900

    Show last 3 days including today

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,8 @@
 class WelcomeController < ApplicationController
   def index
-    @images = RameshImage.order(image_datetime: :desc).group_by do |image|
+    origin = Time.now
+
+    @images = RameshImage.where(image_datetime: (origin - 2.days).beginning_of_day..origin).order(image_datetime: :desc).group_by do |image|
       image.image_datetime.strftime("%Y-%m-%d")
     end
   end
