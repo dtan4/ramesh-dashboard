c6004af2261c6ac454428ca857dd367ba0796607
dtan4 <dtanshi45@gmail.com>
2014-09-02 02:07:02 +0900

    Show discrete day page

--- app/controllers/ramesh_images_controller.rb
+++ app/controllers/ramesh_images_controller.rb
@@ -1,5 +1,17 @@
 class RameshImagesController < ApplicationController
   def show
-    @image = RameshImage.find_by(image_datetime: Time.parse(params[:image_datetime]))
+    image_datetime = params[:image_datetime]
+
+    if image_datetime.length == 8
+      @day = Time.parse(image_datetime)
+
+      @images = RameshImage.where(image_datetime: @day.beginning_of_day..@day.end_of_day).order("image_datetime DESC").group_by do |image|
+        image.image_datetime.hour
+      end
+
+      return render :show_by_day
+    end
+
+    @image = RameshImage.find_by(image_datetime: Time.parse(image_datetime))
   end
 end
--- spec/controllers/ramesh_images_controller_spec.rb
+++ spec/controllers/ramesh_images_controller_spec.rb
@@ -2,23 +2,53 @@ require 'rails_helper'
 
 describe RameshImagesController, :type => :controller do
   describe "GET show" do
-    let(:image_datetime) { "201409012300" }
-    let(:show)           { get :show, image_datetime: image_datetime }
+    let(:show) { get :show, image_datetime: image_datetime }
 
-    before do
-      FactoryGirl.create(:ramesh_image, image_datetime: Time.parse(image_datetime))
+    context "if image_datetime is YYYYmmdd" do
+      let(:image_datetime) { "20140901" }
+
+      before do
+        origin = Time.new(2014, 9, 1, 0, 5, 0)
+
+        [0, 5, 10].each do |minutes_ago|
+          FactoryGirl.create(:ramesh_image, image_datetime: origin - minutes_ago.minutes)
+        end
+      end
+
+      it "returns http success" do
+        show
+        expect(response).to be_success
+      end
+
+      it "render show_by_day template" do
+        expect(show).to render_template :show_by_day
+      end
+
+      it "assigns images" do
+        show
+        images = assigns(:images)
+        expect(images.length).to eq 1
+      end
     end
 
-    it "returns http success" do
-      show
-      expect(response).to be_success
-    end
+    context "if image_datetime is YYYYmmddHHMM" do
+      let(:image_datetime) { "2014090123" }
 
-    it "assigns specified image" do
-      show
-      image = assigns(:image)
-      expect(image).to be_a RameshImage
-      expect(image.image_datetime).to eq Time.parse(image_datetime)
+      before do
+        FactoryGirl.create(:ramesh_image, image_datetime: Time.parse(image_datetime))
+      end
+
+      it "returns http success" do
+        show
+        expect(response).to be_success
+      end
+
+      it "assigns specified image" do
+        show
+        image = assigns(:image)
+        expect(image).to be_a RameshImage
+        expect(image.image_datetime).to eq Time.parse(image_datetime)
+      end
     end
   end
 end
