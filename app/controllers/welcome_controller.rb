017715674b63885cea98825f961c8b3f52bf2f2a
dtan4 <dtanshi45@gmail.com>
2014-09-01 22:24:32 +0900

    Group images by date

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
-    @images = RameshImage.order(image_datetime: :desc)
+    @images = RameshImage.order(image_datetime: :desc).group_by do |image|
+      image.image_datetime.strftime("%Y-%m-%d")
+    end
   end
 end
