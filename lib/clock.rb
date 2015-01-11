1867fc17e3fc0e480ff3d7ea85ac00cd77e3d7bc
Daisuke Fujita <dtanshi45@gmail.com>
2015-01-11 23:58:55 +0900

    Remove useless require

--- lib/clock.rb
+++ lib/clock.rb
@@ -4,8 +4,6 @@ require File.expand_path('../../config/environment', __FILE__)
 # TODO: to be more sophisticated
 require 'jobs/post_image_job'
 
-require 'clockwork'
-
 include Clockwork
 
 every(5.minutes, "Post most recent image") { PostImageJob.post_image(5) }
