7834d40780d06121afa1c54b635d751d2ba35f5a
Daisuke Fujita <dtanshi45@gmail.com>
2015-10-04 21:53:18 +0900

    Use AWS_DEFAULT_REGION

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,7 @@ CarrierWave.configure do |config|
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'] || ENV['S3_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'] || ENV['S3_SECRET_ACCESS_KEY'],
-      region: ENV['AWS_REGION'] || ENV['S3_REGION'],
+      region: ENV['AWS_DEFAULT_REGION'] || ENV['AWS_REGION'] || ENV['S3_REGION'],
       path_style: true,
     }
 
